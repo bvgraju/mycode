7d63d4cf2281b4ad73f0e07044f0d722a7d5c05a
Raju <bvgraju@users.noreply.github.com>
2022-07-12 20:50:57 +0000

    while looping

---
+++ cust_if_script.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+marks = int(input("What percentage marks you got?" ))
+
+if marks >= 100:
+    print("We know you nerd")
+elif marks >= 90 and marks <= 100:    
+    print("You got A Grade")
+elif marks >= 80:
+    print("You got B Grade")
+elif marks >= 70:
+    print("You got C Grade")
+elif marks >= 60:
+    print("You got D Grade")
+elif marks >= 70:
+    print("You got E Grade")
+elif marks >= 60:
+    print("You got F Grade")
+else:
+    print("You are eligible to run for the president!!!")
+
+    
