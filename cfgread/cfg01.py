9c403797be5a6b51a879be274073537f19b7a157
Raju <bvgraju@users.noreply.github.com>
2022-07-13 19:03:31 +0000

    reading in data

---
+++ cfgread/cfg01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+configfile = open("vlanconfig.cfg", "r")
+print(configfile.read())
+configfile.close()
+
+configfile = open("vlanconfig.cfg", "r")
+configlist = configfile.readlines()
+print(configlist)
+
+for x in configlist:
+    print(x.strip(), end="")
+
+configfile.close()
