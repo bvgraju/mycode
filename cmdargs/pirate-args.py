f9fc8b24a453fa0265acb43cb06fe93cab7f98cb
Raju <bvgraju@users.noreply.github.com>
2022-07-15 19:06:17 +0000

    passing arguments to my script

---
+++ cmdargs/pirate-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" - [https://docs.python.org/3/library/argparse.html](https://docs.python.org/3/library/argparse.html)
+"""
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   sys - basic argument parsing"""
+
+# standard libarary import
+import sys
+
+args = sys.argv                     # define an args object from values passed in during runtime
+print("Username: " + args[1])       # the value in the 1st position is the 1st argument
+print("Password: " + args[2])
+print("IP Address: " + args[3])
+print("Gateway: " + args[4])
+print("Oh by the way, your script is named:", args[0]) # position 0 is the name of the script called
+
